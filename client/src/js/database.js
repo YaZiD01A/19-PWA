06649f57e895821750099418b8fbd6e885b59a40
Yazid <yalhujery01@gmail.com>
2022-04-05 22:34:51 -0400

    push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const jateDB = await openDB("jate", 1);
+  const record = jateDB.transaction("jate", "readwrite")
+  const store = record.objectStore("jate");
+  const request = store.put({id:1, value:content})
+  const answer = await request
+  console.log("data has been saved",answer.value)
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const record = jateDB.transaction("jate", "readonly")
+  const store = record.objectStore("jate");
+  const request = store.get(1)
+  const answer = await request
+  if(answer){
+    console.log("data was accessed", answer.value)
+  }
+  else {
+    console.log("data not found")
+  }
+  return answer?.value
+}
 
 initdb();
